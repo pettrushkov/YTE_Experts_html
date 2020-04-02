13f3b715887f68011dd6c58369c713a5e974d3a9
Denys Petrushkov <denis.p@cheitgroup.com>
2020-04-02 21:09:02 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 jQuery(document).ready(function($) {
     
-   
+   $('.reviews__slider').slick({
+        dots: false,
+        slidesToShow: 2,
+        prevArrow: $('.reviews__slider-buttons-prev'),
+        nextArrow: $('.reviews__slider-buttons-next')
+   });
 
 });
 
